d2e2f8f3e39cdb49aacbd072ff45f233784918df
Alexander Chaschin <ac@happynumbers.com>
2020-04-17 02:00:53 +0300

    add ideal weight calculator

---
+++ 2-Basics/ideal_weight.rb
@@ -0,0 +1,10 @@
+print "What is your name? "
+name = gets.chomp.capitalize
+
+print "What is your height? "
+height = gets.chomp.to_i
+
+weight_ideal = (height - 110) * 1.15
+
+print "#{name}, your weight is "
+puts weight_ideal > 0 ? weight_ideal.to_s : "already optimal"
