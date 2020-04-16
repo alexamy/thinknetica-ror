4bfefa4a9949d15176df167b0321e2513a02fbaa
Alexander Chaschin <ac@happynumbers.com>
2020-04-17 02:05:43 +0300

    add triangle area calculator

---
+++ 2-Basics/triangle_area.rb
@@ -0,0 +1,8 @@
+print "Enter side of triangle: "
+side = gets.chomp.to_i
+
+print "Enter height of triangle: "
+height = gets.chomp.to_i
+
+area = side * height / 2.0
+puts "Triangle area is #{area}"
