e77a0f29d317aea824280652b6e920190c44b224
Alexander Chaschin <ac@happynumbers.com>
2020-04-18 13:05:13 +0300

    change directory structure remove free-course files

--- free-lessons/1-Basics/gets.rb
+++
@@ -1,6 +0,0 @@
-puts "What is your name?"
-name = gets.chomp
-puts "What is your birth year?"
-year = gets.chomp.to_i
-
-puts "Hello, #{name}, have a good day! You age is #{2020 - year}."
--- free-lessons/1-Basics/playground.rb
+++
@@ -1,7 +0,0 @@
-class String
-  def - other
-    self.gsub other, ''
-  end
-end
-
-puts "abaab" - "a"
