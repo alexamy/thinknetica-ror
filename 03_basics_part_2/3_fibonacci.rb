e0bb4448a0ae3cfe7242a7162665485cee09f47c
Alexander Chaschin <ac@happynumbers.com>
2020-04-18 22:34:53 +0300

    refactor fibonacci

--- 03_basics_part_2/3_fibonacci.rb
+++ 03_basics_part_2/3_fibonacci.rb
@@ -1,7 +1,2 @@
-fibonacci_numbers = []
-
-steps = [0, 1]
-100.times do
-  fibonacci_numbers << steps.last
-  steps = [steps.last, steps.first + steps.last]
-end
+numbers = [0, 1]
+100.times { numbers << numbers.last(2).sum }
