e9ce035c87cc283842e7a3a0fc0a9f7f78cbeb71
Alexander Chaschin <ac@happynumbers.com>
2020-04-22 01:50:05 +0300

    add train class

--- 04_oop/railway.rb
+++ 04_oop/railway.rb
@@ -3,16 +3,76 @@ module TrainType
   PASSENGER = 2
 end
 
+# train
 class Train
-  attr_accessor :number, :type, :carriages_count
+  attr_accessor :number, :type, :carriages_count,
+                :velocity, :route, :station_index
 
   def initialize(number, type, carriages_count)
     @number = number
     @type = type
     @carriages_count = carriages_count
+    @velocity = 0
+  end
+
+  def add_velocity(gain = 10)
+    velocity += gain
+  end
+
+  def stop
+    velocity = 0
+  end
+
+  def stopped?
+    velocity == 0
+  end
+
+  def has_carriage?
+    carriages_count > 0
+  end
+
+  def carriage_add
+    carriages_count += 1 if stopped?
+  end
+
+  def carriage_remove
+    carriages_count -= 1 if stopped? && has_carriage?
+  end
+
+  def set_route(route_target)
+    route = route_target
+    station_index = 0
+    route.stations[station_index].add_train(self)
+  end
+
+  def go_to_next_station
+    if route
+      route.stations[station_index].departure(self)
+      station_index += 1
+      route.stations[station_index].add_train(self)
+    end
+  end
+
+  def station_previous
+    if route && station_index > 0
+      route.stations[station_index - 1]
+    end
+  end
+
+  def station_current
+    if route
+      route.stations[station_index]
+    end
+  end
+
+  def station_next
+    if route && station_index < route.stations.size - 1
+      route.stations[station_index + 1]
+    end
   end
 end
 
+# route
 class Route
   attr_accessor :stations
 
@@ -27,12 +87,9 @@ class Route
   def remove_station(station)
     stations.delete(station)
   end
-
-  def show_stations
-    puts stations
-  end
 end
 
+# station
 class Station
   attr_accessor :name, :trains
 
@@ -45,15 +102,11 @@ class Station
     trains << train
   end
 
-  def train_departure
-    trains.pop
-  end
-
-  def trains_available
-    trains
+  def departure(train)
+    trains.delete(train)
   end
 
   def trains_available_by_type(type)
-    trains.select { |t| t.type == type }
+    trains.select { |t| t.type == type }.size
   end
 end
