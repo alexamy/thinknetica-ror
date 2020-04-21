0def790f55873b694112c6ee65ca3bd4ecf632f8
Alexander Chaschin <ac@happynumbers.com>
2020-04-22 01:23:36 +0300

    fix route class

--- 04_oop/railway.rb
+++ 04_oop/railway.rb
@@ -14,18 +14,30 @@ class Train
 end
 
 class Route
-  attr_accessor :stations, :trains
+  attr_accessor :stations
 
   def initialize(station_start, station_end)
     @stations = [station_start, station_end]
-    @trains = []
+  end
+
+  def add_station(station)
+    stations.insert(-2, station)
+  end
+
+  def remove_station(station)
+    stations.delete(station)
+  end
+
+  def show_stations
+    puts stations
   end
 end
 
 class Station
-  attr_accessor :name
+  attr_accessor :name, :trains
 
   def initialize(name)
     @name = name
+    @trains = []
   end
 end
