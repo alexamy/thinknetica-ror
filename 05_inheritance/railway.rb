e7c877e3b4ee205d9b2973bbd6f8a8be04376205
Alexander Chaschin <ac@happynumbers.com>
2020-04-25 12:17:23 +0300

    add ui methods

--- 05_inheritance/railway.rb
+++ 05_inheritance/railway.rb
@@ -11,7 +11,7 @@ class Railway
 
   # - Просматривать список станций и список поездов на станции
 
-  attr_reader stations, routes, trains
+  attr_reader :stations, :routes, :trains
 
   def initialize
     @stations = []
@@ -20,36 +20,55 @@ class Railway
   end
 
   def add_train
-    number = get_input('Enter train number').to_i
+    number = get_input('enter train number').to_i
     trains << Train.new(number)
   end
 
   def add_station
-    name = get_input('Enter station name')
+    name = get_input('enter station name')
     stations << Station.new(name)
   end
 
   def add_route
     return 'Add at least 2 stations first' if stations.size < 2
 
-    loop do
-      station_first = choose_station
-      station_last  = choose_station
-      break if station_first != station_last
-    end
-
-    routes << Route.new(station_first, station_last)
+    routes << Route.new(*choose_route_stations)
   end
 
   private
 
-  attr_writer stations, routes, trains
+  attr_writer :stations, :routes, :trains
 
-  def choose_station
+  # :reek:DuplicateMethodCall
+  def choose_route_stations
+    loop do
+      station_first = choose_from(stations)
+      station_last  = choose_from(stations)
+      break if station_first != station_last
+    end
+    [station_first, station_last]
+  end
+
+  def choose_from(collection)
+    loop do
+      print_collection(collection)
+      index = get_input('your option').to_i
+      result = collection[index]
+      if result
+        return result
+      else
+        puts 'Unknown index'
+    end
+  end
+
+  def print_collection(collection)
+    collection.each.with_index do |item, index|
+      puts "#{index}. #{item}"
+    end
   end
 
   def get_input(message)
-    puts "#{message}:"
+    print "#{message.capitalize}: "
     gets.chomp
   end
 end
