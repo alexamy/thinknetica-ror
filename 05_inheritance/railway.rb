9bbb0a9fd2e7e21783941f771805b14d48e86e46
Alexander Chaschin <ac@happynumbers.com>
2020-04-25 11:37:28 +0300

    add add_* methods in railway

--- 05_inheritance/railway.rb
+++ 05_inheritance/railway.rb
@@ -11,9 +11,45 @@ class Railway
 
   # - Просматривать список станций и список поездов на станции
 
+  attr_reader stations, routes, trains
+
   def initialize
     @stations = []
     @routes = []
     @trains = []
+  end
+
+  def add_train
+    number = get_input('Enter train number').to_i
+    trains << Train.new(number)
+  end
+
+  def add_station
+    name = get_input('Enter station name')
+    stations << Station.new(name)
+  end
+
+  def add_route
+    return 'Add at least 2 stations first' if stations.size < 2
+
+    loop do
+      station_first = choose_station
+      station_last  = choose_station
+      break if station_first != station_last
+    end
+
+    routes << Route.new(station_first, station_last)
+  end
+
+  private
+
+  attr_writer stations, routes, trains
+
+  def choose_station
+  end
+
+  def get_input(message)
+    puts "#{message}:"
+    gets.chomp
   end
 end
