28b986cf0c9af0753f58cd8cf3c843811502aad0
Alexander Chaschin <ac@happynumbers.com>
2020-04-25 14:05:02 +0300

    add railway menu

--- 05_inheritance/main.rb
+++ 05_inheritance/main.rb
@@ -5,3 +5,33 @@ require_relative 'carriage'
 require_relative 'seed'
 require_relative 'railway'
 require_relative 'ui'
+
+railway = Railway.new
+menu = [
+  'Stop',
+  'Add train',
+  'Add station',
+  'Add route',
+  'Add carriage',
+  'Remove carriage',
+  'Place train on route',
+  'Move train to next station',
+  'Move train to previous station',
+  'Show stations',
+  'Show trains on station',
+  'Add station to route',
+  'Remove station from route'
+]
+
+loop do
+  puts 'Railway Manager'
+  selected = Ui.choose_from(menu)
+  return if selected == 'Stop'
+
+  method = selected.downcase.gsub(' ', '_')
+  result = railway.public_send(method)
+  puts result
+
+  print 'Press any key'
+  STDIN.getc
+end
