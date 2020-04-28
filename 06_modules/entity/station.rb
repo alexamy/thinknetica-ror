107f8ebe3103b965956e0394a6a9d7ffb8f72d2d
Alexander Chaschin <ac@happynumbers.com>
2020-04-28 18:33:14 +0300

    add station all counter

--- 06_modules/entity/station.rb
+++ 06_modules/entity/station.rb
@@ -1,10 +1,17 @@
 # Station belongs to route and can add and departure trains
 class Station
+  class << self
+    def all
+      @all ||= []
+    end
+  end
+
   attr_reader :name, :trains
 
   def initialize(name)
     @name = name
     @trains = []
+    self.class.all << self
   end
 
   def add_train(train)
