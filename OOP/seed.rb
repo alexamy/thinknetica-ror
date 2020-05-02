6f4f14e836cd76dbd55173e022f3277846a24fda
Alexander Chaschin <ac@happynumbers.com>
2020-05-03 00:38:49 +0300

    add seed iteration method

--- OOP/seed.rb
+++ OOP/seed.rb
@@ -9,6 +9,43 @@ class Seed
     @carriages = Seed.sample_carriages
   end
 
+  def iterate_all
+    stations.each do |station|
+      station.each_train do |train|
+        print_train_info(train)
+        train.each_carriage.with_index(1) do |carriage, index|
+          puts "Carriage #{index} of type #{carriage.class}"
+          print_carriage_info(carriage)
+        end
+      end
+    end
+  end
+
+  def print_train_info(train)
+    puts "Train #{train.number}"
+    puts "has #{trains.carriages.size} carriage(s)"
+    puts "of type #{train.carriage_type}"
+  end
+
+  def print_carriage_info(carriage)
+    data = get_places_data(carriage)
+    puts "Has #{data[:total]} #{prefix} total"
+    puts "with #{data[:occupied]} occupied"
+    puts "and #{data[:free]} free"
+  end
+
+  def get_places_data(carriage)
+    prefixes = { 'CargoCarriage' => 'volume', 'PassengerCarriage' => 'seats' }
+    raise 'Unknown carriage type' unless prefixes.include?(carriage.class)
+
+    prefix = prefixes[carriage.class]
+    {
+      total: carriage.send(prefix),
+      occupied: carriage.send("#{prefix}_occupied"),
+      free: carriage.send("#{prefix}_free"),
+    }
+  end
+
   def self.sample_stations
     %w[Stockton Minneapolis Garland Seattle Jacksonville]
       .map { |station| Station.new(station) }
