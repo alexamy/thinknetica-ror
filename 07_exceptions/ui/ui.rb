bc0e8a9c602ec745072e33fd51a2c9a5f7df1665
Alexander Chaschin <ac@happynumbers.com>
2020-05-02 03:08:48 +0300

    simplify ui

--- 07_exceptions/ui/ui.rb
+++ 07_exceptions/ui/ui.rb
@@ -1,32 +1,21 @@
 # UI helper functions
 module Ui
   def self.choose_from(collection)
-    choose_from!(collection)
+    select_in_collection(collection)
   rescue StandardError => e
     puts e.message
     retry
   end
 
-  def self.choose_from!(collection)
-    result = select_in_collection(collection)
-    raise 'Unknown index' unless result
-
-    result
-  end
-
   def self.select_in_collection(collection)
     print_collection(collection)
     index = get_input('your option').to_i
+    raise 'Unknown index' unless index.between?(0, collection.size - 1)
+
     collection[index]
   end
 
   def self.print_collection(collection)
-    print_collection!(collection)
-  rescue StandardError => e
-    puts e.message
-  end
-
-  def self.print_collection!(collection)
     raise 'No elements' if collection.empty?
 
     collection.each.with_index do |item, index|
