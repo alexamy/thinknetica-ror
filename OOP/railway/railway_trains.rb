6ddbbe588f11f1235f211996e91f09808b8cc468
Alexander Chaschin <ac@happynumbers.com>
2020-05-03 12:39:52 +0300

    add select train method

--- OOP/railway/railway_trains.rb
+++ OOP/railway/railway_trains.rb
@@ -12,36 +12,26 @@ module RailwayTrains
   end
 
   def add_carriage_to_train
-    validate! :trains
-
-    train = Ui.choose_from(trains)
+    train = select_train
     carriage = train.carriage_type
     amount = Ui.get_input("enter #{carriage.space_key} amount")
     train.add_carriage(carriage.new(amount))
   end
 
   def remove_carriage_from_train
-    validate! :trains
-
-    train = Ui.choose_from(trains)
+    train = select_train
     train.remove_carriage
   end
 
   def use_space_in_carriage
-    validate! :trains
-
-    train = Ui.choose_from(trains)
+    train = select_train
     carriages = train.carriages
 
     raise 'No carriages!' if carriages.empty?
 
     carriage = Ui.choose_from(carriages)
     space_data = carriage.space_data
-
-    raise 'All space occupied!' if space_data[:free].zero?
-
-    key = space_data[:name]
-    method = "occupy_#{key}".to_sym
+    method = "occupy_#{space_data[:name]}".to_sym
 
     if key == 'seats'
       carriage.send(method)
@@ -60,18 +50,14 @@ module RailwayTrains
   end
 
   def move_train_to_next_station
-    validate! :trains
-
-    train = Ui.choose_from(trains)
+    train = select_train
     route_assigned!(train)
 
     train.go_to_next_station
   end
 
   def move_train_to_previous_station
-    validate! :trains
-
-    train = Ui.choose_from(trains)
+    train = select_train
     route_assigned!(train)
 
     train.go_to_previous_station
@@ -83,6 +69,11 @@ module RailwayTrains
     Ui.print_collection(trains) do |train|
       train.each_carriage { |carriage| puts carriage }
     end
+  end
+
+  def select_train
+    validate! :trains
+    Ui.choose_from(trains)
   end
 
   protected
