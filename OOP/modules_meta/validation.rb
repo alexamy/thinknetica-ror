74c6754250d7021f5e291e29287d36ab71237b42
Alexander Chaschin <ac@happynumbers.com>
2020-05-05 16:35:31 +0300

    add has_validator check

--- OOP/modules_meta/validation.rb
+++ OOP/modules_meta/validation.rb
@@ -2,16 +2,35 @@
 module Validation
   def self.extended(base)
     base.class_eval do
+      instance_variable_set(:@validations, [])
       add_validate
       add_valid
     end
   end
 
-  def validate(attr, type, *args)
+  def validate(name, type, *args)
+    raise 'Validation already presented' if has_validator?(name, type)
+
     puts 'add validator'
   end
 
   protected
+
+  def has_validator?(name, type)
+    validator = @validations.find do |opts|
+      opts[:attr] == name && opts[:type] == type
+    end
+    !!validator
+  end
+
+  def validate_presence
+  end
+
+  def validate_format
+  end
+
+  def validate_type
+  end
 
   def add_validate
     define_method :validate! do
